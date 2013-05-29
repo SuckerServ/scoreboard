40fca4ec022137fa49e87cee53f494b1f32097b9
aaron@graphox.us <aaron@graphox.us@32555feb-acdc-9533-d4e0-d00e80c872cf>
2013-05-29 10:05:08 +0000

    some minor scoreboard changes
    git-svn-id: https://suckerserv.googlecode.com/svn/trunk/scoreboard@557 32555feb-acdc-9533-d4e0-d00e80c872cf

--- index.php
+++ index.php
@@ -1,40 +1,48 @@
 <?php
-$pagename = "scoreboard";
-include("includes/geoip.inc");
-include("includes/hopmod.php");
+  $pagename = "scoreboard";
+  include("includes/geoip.inc");
+  include("includes/hopmod.php");
 
-if (! isset($_SESSION['querydate']) ) { $_SESSION['querydate'] = "month";}
-if (! isset($_GET['page']) ) { $_GET['page'] = 0;}
+  if ( ! isset($_SESSION['querydate'])) {
+    $_SESSION['querydate'] = "month";
+  }
+  if ( ! isset($_GET['page'])) {
+    $_GET['page'] = 0;
+  }
+
 // Setup statsdb and assign it to an object.
-$dbh = setup_pdo_statsdb($db);
+  $dbh = setup_pdo_statsdb($db);
 
-$dat = explode(",",date('n,Y')); 
-$year = date('Y');
-$week = date('W');
+  $times = array (
+    "year" => date('Y'),
+    "week" => date('W'),
+    "end" => array (
+      "day" => strtotime("today +1 day"),
+      "week" => strtotime(date('Y') . "-W" . date('W') . "-7"),
+      "month" => mktime(23, 59, 0, date("n"), date("t")),
+      "year" => strtotime("today"),
+      "nolimit" => strtotime("today +1 day"),
+    ),
+    "start" => array (
+      "day" => strtotime("today"),
+      "week" => strtotime(date('Y') . "-W" . date('W') . "-1"),
+      "month" => mktime(0, 0, 0, date("n"), 1),
+      "year" => strtotime("today -365 days"),
+      "nolimit" => "0",
+    )
+  );
 
-$day_end = strtotime("today +1 day");
-$week_end = strtotime("{$year}-W{$week}-7");
-$month_end = mktime(23,59,59,$dat[0]+1,0,$dat[1]);
-$year_end = strtotime("today");
-$nolimit_end = strtotime("today +1 day");
+  $_SESSION['end_date'] = $times["end"][$_SESSION['querydate']];
+  $_SESSION['start_date'] = $times["start"][$_SESSION['querydate']];
 
-$day_start = strtotime("today");
-$week_start = strtotime("{$year}-W{$week}-1");
-$month_start = mktime(0,0,0,$dat[0],1,$dat[1]);;
-$year_start = strtotime("today -365 days");
-$nolimit_start = "0";
+  $day = "";
 
-$_SESSION['start_date'] = ${$_SESSION['querydate']."_start"};
-$_SESSION['end_date'] = ${$_SESSION['querydate']."_end"};
-
-$day = "";
-
-$sql = $dbh->prepare("
+  $sql = $dbh->prepare("
         select *
         from
             (select name,
-                country as PlayerCountry,
                 ipaddr as PlayerIP,
+                country as PlayerCountry,
                 sum(score) as TotalScored,
                 sum(teamkills) as TotalTeamkills,
                 max(frags) as MostFrags,
@@ -48,10 +56,10 @@ $sql = $dbh->prepare("
             where
                 UNIX_TIMESTAMP(games.datetime) between :start_date and :end_date and frags > 0
             group by name
-            order by ".$_SESSION['orderby']." desc) T
-        where TotalGames >= :MinimumGames limit ".$_SESSION['paging'].",".$rows_per_page);
+            order by " . $_SESSION['orderby'] . " desc) T
+        where TotalGames >= :MinimumGames limit " . $_SESSION['paging'] . "," . $rows_per_page);
 
-$pager_query = $dbh->prepare("
+  $pager_query = $dbh->prepare("
         select COUNT(*)
         from
                 (select name,
@@ -61,27 +69,37 @@ $pager_query = $dbh->prepare("
                         inner join games on players.game_id=games.id
                 where UNIX_TIMESTAMP(games.datetime) between :start_date and :end_date and frags > 0 group by name) T
         where TotalGames >= :MinimumGames");
+
+  $titles = array (
+    "day" => "DAY",
+    "week" => "WEEK",
+    "month" => "MONTH",
+    "year" => "YEAR",
+    "nolimit" => "NO LIMIT"
+  );
 ?>
 
-        <h1><?php print(colorname("$server_title ")); ?> Scoreboard</h1>
+<h1><?php print(colorname("$server_title ")); ?> Scoreboard</h1>
 
-        <div id="filter-panel">
-            <span class="filter-form" style="margin-right:0.5em">
-            <a style="border:0.2em solid; padding:0.5em;margin:0 -0.9em 0 -0.7em;#555555;color:blue; font-weight:bold;font-size:1.1em" href="activity.php">Daily activity</a>
-            </span>
-            <span class="filter-form">
-            Limit to this [ <a href="?querydate=day" <?php if ( $_SESSION['querydate'] == "day" ) { print "class=\"selected\" style=\"color:black\""; } ?>>DAY</a> | 
-            <a href="?querydate=week" <?php if ( $_SESSION['querydate'] == "week" ) { print "class=\"selected\" style=\"color:black\""; } ?>>WEEK</a> | 
-            <a href="?querydate=month" <?php if ( $_SESSION['querydate'] == "month" ) { print "class=\"selected\" style=\"color:black\""; } ?> >MONTH</a> | 
-            <a href="?querydate=year" <?php if ( $_SESSION['querydate'] == "year" ) { print "class=\"selected\" style=\"color:black\""; } ?>>YEAR</a> |
-            <a href="?querydate=nolimit" <?php if ( $_SESSION['querydate'] == "nolimit" ) { print "class=\"selected\" style=\"color:black\""; } ?>>NO LIMIT</a> ]</span>
-            <span class="filter-form"><form id="filter-form">Name Filter: <input name="filter" id="filter" value="" maxlength="30" size="30" type="text"></form></span>
-			<span class="filter-form"><a style="border:0.2em solid; padding:0.5em;margin:0 -0.6em 0 -0.7em;#555555;color:blue; font-weight:bold;font-size:1.1em" href="servers.php">Server list</a></span>
-        </div>
-                <?php $pager_query->execute(array(':start_date' => $_SESSION['start_date'], ':end_date' => $_SESSION['end_date'],':MinimumGames' => $_SESSION['MinimumGames']));
-                      build_pager($_GET['page'],$pager_query,$rows_per_page); //Generate Pager Bar ?>
-        <?php $sql->execute(array(':start_date' => $_SESSION['start_date'], ':end_date' => $_SESSION['end_date'],':MinimumGames' => $_SESSION['MinimumGames']));
-              stats_table($sql); //Build stats table data ?> 
-        <?php stopbench(); //Stop and display benchmark.?>
-    </body>
+<div id="filter-panel">
+  <span class="filter-form" style="margin-right:0.5em">
+    <a style="border:0.2em solid; padding:0.5em;margin:0 -0.9em 0 -0.7em;#555555;color:blue; font-weight:bold;font-size:1.1em" href="activity.php">Daily activity</a>
+  </span>
+  <span class="filter-form">
+    Limit to this [
+    <? foreach ($titles as $title => $display) { ?>
+        <a href="?querydate=<?= $title ?>" <?= $_SESSION['querydate'] == "day" ? "class=\"selected\" style=\"color:black\"" : ""; ?>><?= $display ?></a><?= ($title != "nolimit" ? " | " : "") ?>
+  <? } ?>
+    ]</span>
+  <span class="filter-form"><form id="filter-form">Name Filter: <input name="filter" id="filter" value="" maxlength="30" size="30" type="text"></form></span>
+  <span class="filter-form"><a style="border:0.2em solid; padding:0.5em;margin:0 -0.6em 0 -0.7em;#555555;color:blue; font-weight:bold;font-size:1.1em" href="servers.php">Server list</a></span>
+</div>
+<?php $pager_query->execute(array ( ':start_date' => $_SESSION['start_date'], ':end_date' => $_SESSION['end_date'], ':MinimumGames' => $_SESSION['MinimumGames'] ));
+  build_pager($_GET['page'], $pager_query, $rows_per_page); //Generate Pager Bar 
+?>
+<?php $sql->execute(array ( ':start_date' => $_SESSION['start_date'], ':end_date' => $_SESSION['end_date'], ':MinimumGames' => $_SESSION['MinimumGames'] ));
+  stats_table($sql); //Build stats table data 
+?> 
+<?php stopbench(); //Stop and display benchmark. ?>
+</body>
 </html>
